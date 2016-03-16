70173ebfefd2e1d84ceeb61350f52d5a2a8fe285
Sanjay Singh <sanjaysi@usc.edu>
2016-03-16 12:10:12 -0700

    Britecore Project, First commit

---
+++ env1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ankit/britecore/env1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ featureRequest/models.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    client_a='A'
+    client_b='B'
+    client_c='C'
+    policy='P'
+    bill='B'
+    claim='C'
+    report='R'
+    client_choice=((client_a,'Client A'),(client_b,'Client B'),(client_c,'Client C'),)
+    prod_choice=((policy,'Policies'),(bill,'Billing'),(claim,'Claims'),(report,'Reports'))
+    
+    #fields decalaration
+    user = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)    
+    client = models.CharField(max_length=1,choices=client_choice)
+    client_priority=models.IntegerField()
+    description = models.TextField()    
+    target_date=models.DateField(auto_now=False, auto_now_add=False)
+    ticket_url=models.URLField()
+    prod_area=models.CharField(max_length=1,choices=prod_choice)   
+
+    def publish(self):       
+        self.save()
+
+    def __str__(self):
+        return self.title
