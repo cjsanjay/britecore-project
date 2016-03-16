60ba68b030ca2ffe5d3625dc579ed6592a63d664
cjsanjay <cjsanjay@gmail.com>
2016-03-16 13:55:21 -0700

    BriteCore project, Displaying data from DB in template dynamically

--- featureRequest/views.py
+++ featureRequest/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'featureRequest/post_list.html',{})
+    posts=Post.objects.all()
+    return render(request,'featureRequest/post_list.html',{'posts':posts})
